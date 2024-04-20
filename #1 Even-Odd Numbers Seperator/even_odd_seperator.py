f3240e4a4b91226111b30a44526e25d32a8ba604
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-20 20:03:04 +0800

    Read the numbers from the number.txt file

--- #1 Even-Odd Numbers Seperator/even_odd_seperator.py
+++ #1 Even-Odd Numbers Seperator/even_odd_seperator.py
@@ -1,7 +1,11 @@
 # pseudocode
 
 # Make a "number.txt" file with 20 integers
+
 # Read the contents of the txt file
+with open("number.txt", "r") as file:
+    numbers = file.read().split()
+
 # Compute the even numbers in the list
 # Compute the odd numbers in the list
 # Create an "even.txt" for even numbers
