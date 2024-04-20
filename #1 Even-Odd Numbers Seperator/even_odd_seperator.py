2fb4c148eff36135c8dd6c775a9ca628ebbe1958
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-20 18:47:12 +0800

    Added a file directory (Even-Odd Numbers Seperator) and a pseudocode

---
+++ #1 Even-Odd Numbers Seperator/even_odd_seperator.py
@@ -0,0 +1,8 @@
+# pseudocode
+
+# Make a "number.txt" file with 20 integers
+# Read the contents of the txt file
+# Compute the even numbers in the list
+# Compute the odd numbers in the list
+# Create an "even.txt" for even numbers
+# Create an "odd.txt" for odd numbers
