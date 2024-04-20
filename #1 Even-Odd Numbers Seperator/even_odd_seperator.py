eab2b998310f32e4f8d4893d007148839600ac37
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-20 20:28:13 +0800

    Convert string from numbers.txt file to integers

--- #1 Even-Odd Numbers Seperator/even_odd_seperator.py
+++ #1 Even-Odd Numbers Seperator/even_odd_seperator.py
@@ -3,10 +3,19 @@
 # Make a "number.txt" file with 20 integers
 
 # Read the contents of the txt file
-with open("number.txt", "r") as file:
+with open("numbers.txt", "r") as file:
     numbers = file.read().split()
 
+# Convert string to integers
+    numbers = (int(num) for num in numbers)
+
 # Compute the even numbers in the list
+even_numbers = []
+for num in numbers:
+    if num % 2 == 0:
+        even_numbers.append(num)
+        print(num)
+
 # Compute the odd numbers in the list
 # Create an "even.txt" for even numbers
 # Create an "odd.txt" for odd numbers
