cdb80e5efc86c9fd405d7537959eee36bfc69295
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-20 20:55:35 +0800

    Created an even.txt file for even numbers

--- #1 Even-Odd Numbers Seperator/even_odd_seperator.py
+++ #1 Even-Odd Numbers Seperator/even_odd_seperator.py
@@ -12,6 +12,7 @@ with open("numbers.txt", "r") as file:
 # Find the even and odd numbers in the list
 even_numbers = []
 odd_numbers = []
+
 for num in numbers:
     if num % 2 == 0:
         even_numbers.append(num)
@@ -19,4 +20,9 @@ for num in numbers:
         odd_numbers.append(num)
 
 # Create an "even.txt" for even numbers
+with open("even.txt", "w") as even_file:
+    even_file.write("Here is the even numbers: \n")
+    for num in even_numbers:
+        even_file.write(str(num) + "\n")
+
 # Create an "odd.txt" for odd numbers
