cbadd468361fa50445de3e4a406022cc7f8bae34
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-21 23:54:17 +0800

    Find the name and GWA from recognizing the row

--- #2 Highest GWA Finder/highest_gwa_finder.py
+++ #2 Highest GWA Finder/highest_gwa_finder.py
@@ -7,14 +7,21 @@ import pandas as pd
 students_gwa = pd.read_excel("students_gwa.xlsx")
 
 # Find the student with highest GWA
+# Sorts the GWA (1.00 being the highest)
 highest_to_lowest = (
     students_gwa
     .sort_values("GWA", ascending=True)
     .to_string(index=False)
-) # Sorts the GWA (1.00 being the highest)
+)
+
+# Locate the first row
 highest_gwa_row = (highest_to_lowest.split("\n")[1])
 
+# Split the row into individual elements
+elements = highest_gwa_row.split()
 
-
+# Declaring the second and third element
+name = " ".join(elements[:-1])
+gwa = elements[-1]
 
 # Display the student and their GWA
