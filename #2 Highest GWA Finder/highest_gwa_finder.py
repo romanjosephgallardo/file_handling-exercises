eaa54ef9fd2412b7bfef3f789eca1d6b9ef2e671
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-21 22:44:49 +0800

    Installed pandas module and import it on the program file

--- #2 Highest GWA Finder/highest_gwa_finder.py
+++ #2 Highest GWA Finder/highest_gwa_finder.py
@@ -1,7 +1,9 @@
 # pseudocode
 
-# Have a file with 20 students and their GWA
+# Have a file with 20 students and their GWA (done)
 
 # Read the file
+import pandas as pd
+
 # Find the student with highest GWA
 # Display the student and their GWA
