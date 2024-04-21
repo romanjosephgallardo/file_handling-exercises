3977d573562b07535e96ecbb6749cd2fa3b538f6
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-21 23:24:27 +0800

    Starting to find the highest GWA by sorting in ascending

--- #2 Highest GWA Finder/highest_gwa_finder.py
+++ #2 Highest GWA Finder/highest_gwa_finder.py
@@ -5,6 +5,15 @@
 # Read the file
 import pandas as pd
 students_gwa = pd.read_excel("students_gwa.xlsx")
-print(students_gwa)
+
 # Find the student with highest GWA
+highest_to_lowest = (
+    students_gwa
+    .sort_values("GWA", ascending=True)
+    .to_string(index=False)
+) # Sorts the GWA (1.00 being the highest)
+
+
+
+
 # Display the student and their GWA
