ab661f68d95ea32cee05d2a12cfcf9d5c760eeef
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-21 22:13:47 +0800

    Revised the pseudocode :D

--- #2 Highest GWA Finder/highest_gwa_finder.py
+++ #2 Highest GWA Finder/highest_gwa_finder.py
@@ -1,6 +1,6 @@
 # pseudocode
 
-# Have a txt file with 20 students and their GWA
-# Read the txt.file
+# Have a file with 20 students and their GWA
+# Read the file
 # Find the student with highest GWA
 # Display the student and their GWA
