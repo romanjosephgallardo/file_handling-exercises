3ffe2dfd835cb44bae9e2c9c9566f58b5d85e508
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 00:50:15 +0800

    Added a function to improve the design of output

--- #2 Highest GWA Finder/highest_gwa_finder.py
+++ #2 Highest GWA Finder/highest_gwa_finder.py
@@ -2,8 +2,18 @@
 
 # Have a file with 20 students and their GWA (done)
 
+def print_message(message, another_message):
+    horizontal_border = "=" * 50
+    formatted_message = (f"{horizontal_border}\n\n"
+                        f"{message.center(50)}\n"
+                        f"{another_message.center(50)}\n\n"
+                        f"{horizontal_border}")
+    print(formatted_message)
+
+
 # Read the file
 import pandas as pd
+
 student_data = pd.read_excel("students_gwa.xlsx")
 
 # Find the student with highest GWA
@@ -14,6 +24,6 @@ student_name = highest_gwa.iloc[0]
 student_gwa = highest_gwa.iloc[1]
 
 # Display the student and their GWA
-print("Student with the highest GWA:")
-print(f"Name: {student_name}")
-print(f"GWA: {student_gwa}")
+output_message = "Student with the highest GWA:"
+name_and_gwa = f"{student_name} — {student_gwa}"
+print_message(output_message, name_and_gwa)
