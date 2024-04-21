23fad38bb2daf2763cf4563b97d35189db5f82eb
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 00:52:45 +0800

    Tweaking some changes

--- #2 Highest GWA Finder/highest_gwa_finder.py
+++ #2 Highest GWA Finder/highest_gwa_finder.py
@@ -2,12 +2,12 @@
 
 # Have a file with 20 students and their GWA (done)
 
-def print_message(message, another_message):
+def print_message(first_line, second_line):
     horizontal_border = "=" * 50
     formatted_message = (f"{horizontal_border}\n\n"
-                        f"{message.center(50)}\n"
-                        f"{another_message.center(50)}\n\n"
-                        f"{horizontal_border}")
+                         f"{first_line.center(50)}\n"
+                         f"{second_line.center(50)}\n\n"
+                         f"{horizontal_border}")
     print(formatted_message)
 
 
@@ -26,4 +26,5 @@ student_gwa = highest_gwa.iloc[1]
 # Display the student and their GWA
 output_message = "Student with the highest GWA:"
 name_and_gwa = f"{student_name} — {student_gwa}"
+
 print_message(output_message, name_and_gwa)
