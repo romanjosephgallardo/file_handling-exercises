a6b7d90d3ba4f314040cc72cd5da53a5f5defbe9
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 17:59:18 +0800

    Added whitespace

--- #3 Python Text File Writer/user_input_text_to_file.py
+++ #3 Python Text File Writer/user_input_text_to_file.py
@@ -3,6 +3,7 @@ into a text file. Users can enter a string each line
 until finished with prompts guiding the process.
 """
 
+
 # Define a function for horizontal line
 def horizontal_border():
     print("=" * 50, "\n")
