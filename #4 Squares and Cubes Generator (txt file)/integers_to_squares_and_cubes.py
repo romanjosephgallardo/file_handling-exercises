7f55524268e6e1b55ee3854c1ed2e4f4780fefce
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 18:17:36 +0800

    Added a file folder for the last program (Squares and Cubes Generator) and its pseudocode

---
+++ #4 Squares and Cubes Generator (txt file)/integers_to_squares_and_cubes.py
@@ -0,0 +1,6 @@
+# pseudocode
+
+# Read the contents of the source text file
+# Create double.txt file containing squares of even integers
+# Create triple.txt file containing cubes of odd numbers
+# Shows an output where the files are already created
