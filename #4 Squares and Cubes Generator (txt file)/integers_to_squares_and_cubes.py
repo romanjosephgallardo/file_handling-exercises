4705f74b0937f6af9cfbe345722c59553f2bcdcf
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 19:30:10 +0800

    Created a triple.txt file for odd numbers

--- #4 Squares and Cubes Generator (txt file)/integers_to_squares_and_cubes.py
+++ #4 Squares and Cubes Generator (txt file)/integers_to_squares_and_cubes.py
@@ -14,7 +14,7 @@ for num in integers:
     if num % 2 == 0:
         even_numbers.append(num ** 2)
     else:
-        cubed_numbers = num ** 3
+        odd_numbers.append(num ** 3)
 
 # Create double.txt file containing squares of even integers
 with open("double.txt", "w") as double_file:
@@ -23,4 +23,9 @@ with open("double.txt", "w") as double_file:
         double_file.write(str(num) + "\n")
 
 # Create triple.txt file containing cubes of odd numbers
+with open("triple.txt", "w") as triple_file:
+    triple_file.write("Here are the cube of all odd integers: \n")
+    for num in odd_numbers:
+        triple_file.write(str(num) + "\n")
+
 # Shows an output where the files are already created
