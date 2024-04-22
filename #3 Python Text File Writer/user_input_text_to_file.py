fd45a6b52e1f3c74d822cda9b83ad4749683ebdf
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 15:15:24 +0800

    Testing to enter a line

--- #3 Python Text File Writer/user_input_text_to_file.py
+++ #3 Python Text File Writer/user_input_text_to_file.py
@@ -1,10 +1,11 @@
 # pseudocode
 
 # Create a text file
-with open("mylife.txt", "a") as file:
-    file.write("Test")
+with open("mylife.txt", "w") as file:
 
 # Prompt user to input a line of text
+    line = input("Enter line: ")
+    file.write(line + "\n")
 # Ask user where there are more lines to enter
 # If no more lines, end the program.
 
