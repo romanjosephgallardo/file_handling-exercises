2ae0a8e73280b5abb735ba0fcb3dff7fa45082bf
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 15:51:32 +0800

    Handling issues if a user enters other characters rather than y or n

--- #3 Python Text File Writer/user_input_text_to_file.py
+++ #3 Python Text File Writer/user_input_text_to_file.py
@@ -6,10 +6,14 @@ with open("mylife.txt", "w") as file:
         # Prompt user to input a line of text
         line = input("Enter line: ")
         file.write(line + "\n")
-
-        # Ask user where there are more lines to enter
-        more_lines = input("Are there more lines? (y/n): ")
-        if more_lines.lower() != "y":
-            # If no more lines, end the program.
+        while True:
+            # Ask user where there are more lines to enter
+            more_lines = input("Are there more lines? (y/n): ")
+            if more_lines.lower() == "y" or more_lines.lower() == "n":
+                break
+            else:
+                print("Invalid input. Please enter 'y' or 'n' only.")
+        # If no more lines, end the program.
+        if more_lines.lower() == "n":
             print("Contents written to mylife.txt successfully! :>")
             break
