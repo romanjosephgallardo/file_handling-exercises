14244078853ca799e5fb457d4979d0f3eac31349
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 18:50:22 +0800

    Locating the even and odd numbers from the txt file

--- #4 Squares and Cubes Generator (txt file)/integers_to_squares_and_cubes.py
+++ #4 Squares and Cubes Generator (txt file)/integers_to_squares_and_cubes.py
@@ -6,6 +6,16 @@ with open("integers.txt", "r") as file:
     # Convert string to integers
     integers = (int(num) for num in integers)
 
+# Find the even and odd numbers in the txt file
+even_numbers = []
+odd_numbers = []
+
+for num in integers:
+    if num % 2 == 0:
+        squared_numbers = num ** 2
+    else:
+        cubed_numbers = num ** 3
+
 # Create double.txt file containing squares of even integers
 # Create triple.txt file containing cubes of odd numbers
 # Shows an output where the files are already created
