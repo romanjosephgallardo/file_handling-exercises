92c1405fc8a71cc3d9d2123df41803634713c6e5
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 18:26:56 +0800

    Testing to read the integers.txt file

--- #4 Squares and Cubes Generator (txt file)/integers_to_squares_and_cubes.py
+++ #4 Squares and Cubes Generator (txt file)/integers_to_squares_and_cubes.py
@@ -1,6 +1,9 @@
 # pseudocode
 
 # Read the contents of the source text file
+with open("integers.txt", "r") as file:
+    numbers = file.read().split()
+    print(numbers)
 # Create double.txt file containing squares of even integers
 # Create triple.txt file containing cubes of odd numbers
 # Shows an output where the files are already created
