1c6c58712fcc4f23f8f5bb8ac97387830b69b63b
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 16:58:00 +0800

    Added some styling for output

--- #3 Python Text File Writer/user_input_text_to_file.py
+++ #3 Python Text File Writer/user_input_text_to_file.py
@@ -1,4 +1,15 @@
-# pseudocode
+# Define a function for horizontal line
+def horizontal_border():
+    print("=" * 50, "\n")
+
+
+# Define a function for printing specific outputs
+def print_message(message):
+    formatted_output = f">> {message}"
+    print(formatted_output)
+
+
+horizontal_border()  # Calls the horizontal border function
 
 # Create a text file
 with open("mylife.txt", "w") as file:
@@ -6,7 +17,7 @@ with open("mylife.txt", "w") as file:
         # Prompt user to input a line of text
         line = input("Enter line: ")
         if not line.strip():  # Check if line is empty or whitespace only
-            print("Invalid line. Please enter a non-empty line.")
+            print_message("Invalid line. Please enter a non-empty line.")
             continue
         file.write(line + "\n")
 
@@ -16,8 +27,10 @@ with open("mylife.txt", "w") as file:
             if more_lines.lower() == "y" or more_lines.lower() == "n":
                 break
             else:  # Prompts the user for invalid input
-                print("Invalid input. Please enter 'y' or 'n' only.")
+                print_message("Invalid input. Please enter 'y' or 'n' only.")
         # If no more lines, end the program.
         if more_lines.lower() == "n":
-            print("Contents written to file successfully! :>")
+            print_message("Contents written to file successfully! :> \n")
             break
+
+horizontal_border()
