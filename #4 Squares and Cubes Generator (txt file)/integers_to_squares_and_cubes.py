9e507000825e2da1b284261ea75e6b6d2e508c77
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 18:32:14 +0800

    Convert the string to integers from the file

--- #4 Squares and Cubes Generator (txt file)/integers_to_squares_and_cubes.py
+++ #4 Squares and Cubes Generator (txt file)/integers_to_squares_and_cubes.py
@@ -2,8 +2,10 @@
 
 # Read the contents of the source text file
 with open("integers.txt", "r") as file:
-    numbers = file.read().split()
-    print(numbers)
+    integers = file.read().split()
+    # Convert string to integers
+    integers = (int(num) for num in integers)
+
 # Create double.txt file containing squares of even integers
 # Create triple.txt file containing cubes of odd numbers
 # Shows an output where the files are already created
