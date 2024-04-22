516245c96fb4b3fa5aa2e0a6b4f806c92c31f871
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 15:31:04 +0800

    Added while True for looping

--- #3 Python Text File Writer/user_input_text_to_file.py
+++ #3 Python Text File Writer/user_input_text_to_file.py
@@ -2,14 +2,14 @@
 
 # Create a text file
 with open("mylife.txt", "w") as file:
+    while True:
+        # Prompt user to input a line of text
+        line = input("Enter line: ")
+        file.write(line + "\n")
 
-# Prompt user to input a line of text
-    line = input("Enter line: ")
-    file.write(line + "\n")
-
-# Ask user where there are more lines to enter
-    more_lines = input("Are there more lines? (y/n): ")
-    if more_lines.lower() != "y":
-
-# If no more lines, end the program.
-        print("Contents written to mylife.txt successfully! :>")
+        # Ask user where there are more lines to enter
+        more_lines = input("Are there more lines? (y/n): ")
+        if more_lines.lower() != "y":
+            # If no more lines, end the program.
+            print("Contents written to mylife.txt successfully! :>")
+            break
