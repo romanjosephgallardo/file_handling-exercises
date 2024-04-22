abec2fa2215c2d8ecc06309b6678caa9e77141ea
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 15:09:04 +0800

    Testing if the program can create file

--- #3 Python Text File Writer/user_input_text_to_file.py
+++ #3 Python Text File Writer/user_input_text_to_file.py
@@ -1,6 +1,9 @@
 # pseudocode
 
 # Create a text file
+with open("mylife.txt", "a") as file:
+    file.write("Test")
+
 # Prompt user to input a line of text
 # Ask user where there are more lines to enter
 # If no more lines, end the program.
