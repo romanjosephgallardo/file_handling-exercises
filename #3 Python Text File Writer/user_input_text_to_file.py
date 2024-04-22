fd83311ded673d39fd54d6999ce1913c8a0523e2
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-04-22 15:27:02 +0800

    Ask user if there are more lines (end the program if none)

--- #3 Python Text File Writer/user_input_text_to_file.py
+++ #3 Python Text File Writer/user_input_text_to_file.py
@@ -6,6 +6,10 @@ with open("mylife.txt", "w") as file:
 # Prompt user to input a line of text
     line = input("Enter line: ")
     file.write(line + "\n")
-# Ask user where there are more lines to enter
-# If no more lines, end the program.
 
+# Ask user where there are more lines to enter
+    more_lines = input("Are there more lines? (y/n): ")
+    if more_lines.lower() != "y":
+
+# If no more lines, end the program.
+        print("Contents written to mylife.txt successfully! :>")
